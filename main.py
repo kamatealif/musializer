b1e132cc506295a5ee6790118b14179ac1992576
kamatealif <kamatealip1@gmail.com>
2026-01-28 22:45:48 +0530

    added some text

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
 from PyQt5.QtCore import QSize, Qt 
 
 import sys 
@@ -10,14 +10,34 @@ class MainWindow(QMainWindow):
         self.setFixedSize(QSize(800, 600))
         self.setStyleSheet("background-color: #10060A")
 
+        # label 
+        self.heading = QLabel("Welcome to the Musializer....")
+        self.heading.setStyleSheet("""
+            QLabel{
+            color: #fff;
+            font-size = 16px;
+            font-weight:bold;
+            }
+        """)
 
-app = QApplication(sys.argv)
+        #  layout management
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.heading, alignment=Qt.AlignmentFlag.AlignHCenter)
+
+        #container widget to hold the layout 
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
 
-window = MainWindow()
-window.show()
+    window = MainWindow()
+    window.show()
 
-app.exec()
+    app.exec()
 
 
 
