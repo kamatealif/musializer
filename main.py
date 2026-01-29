7dbfba8df7a43dbd83fad9ee431106cdc14564b7
kamatealif <kamatealip1@gmail.com>
2026-01-29 22:00:15 +0530

    added music pause unpause

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ BTN_HOVER = (80, 35, 55)
 TEXT_COLOR = (240, 240, 240)
 ACCENT = (180, 80, 120)
 
-MUSIC_DIR = "assets/musics"
-SUPPORTED = (".wav", ".mp3", ".ogg")
+MUSIC_FILE = "/home/alif/Downloads/musics/lofi-hiphop.wav"
 RUNNING = True
 
 if __name__ == "__main__":
     # pygame setup
     pygame.init()
+    pygame.mixer.init()
+
+    # loading the music and setting up the screen
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.mixer.music.load(MUSIC_FILE)
+    pygame.mixer.music.play()
     clock = pygame.time.Clock()
 
     while RUNNING:
@@ -28,6 +32,12 @@ if __name__ == "__main__":
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     RUNNING = False
+
+                if event.key == pygame.K_k:
+                    pygame.mixer.music.pause()
+
+                if event.key == pygame.K_k and event.mod & pygame.KMOD_CTRL:
+                    pygame.mixer.music.unpause()
 
         screen.fill(BG_COLOR)
 
